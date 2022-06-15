248002ae618175e6c0ec4de92a1f749480f47885
TottHam <ottensmann.tim@gmail.com>
2022-06-15 09:52:10 +0000

    changes made from theia lab environment

--- script.js
+++ script.js
@@ -6,12 +6,12 @@ function compute()
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
-
+    document.getElementById("result").innerText=year;
 }
 
 
 function updateRate() 
 {
     var rateval = document.getElementById("rate").value;
-    document.getElementById("rate_val").innerText=rateval;
+    document.getElementById("rate_val").innerText=rateval+"%";
 }
